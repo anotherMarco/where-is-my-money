6d8ac97753ebc5d6567a71031fbd306b9710d96e
marco ulrich <p.ulrichmarco@gmail.com>
2022-11-03 20:18:33 +0100

    Initial commit

---
+++ src/main/java/com/oninek/whereismymoney/WhereIsMyMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.oninek.whereismymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereIsMyMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereIsMyMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oninek/whereismymoney/WhereIsMyMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oninek.whereismymoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereIsMyMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
